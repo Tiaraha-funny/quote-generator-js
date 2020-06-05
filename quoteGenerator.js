f4fd2dc7770845f5ead9920154d7a7236348c32c
Peta Jeannie <Peta.jea@onja.org>
2020-06-05 13:21:23 +0300

    started doing the second question

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -9,3 +9,14 @@ const randomArray_3 = lastArray[Math.floor(Math.random() * lastArray.length)];
 
 let quotes = `${randomArray_1}, ${randomArray_2} and ${randomArray_3} `;
 console.log(quotes);
+
+let genNum = 5;
+for (let number of genNum) {
+  if (number > 1) {
+    return genNum.quotes(1);
+  } else if (number > 5) {
+    return genNum.quotes(2);
+  } else {
+    return genNum.quotes(0);
+  }
+}
