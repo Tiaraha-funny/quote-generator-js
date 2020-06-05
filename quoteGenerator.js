9cf3a410c42922f4b3c339bb72011f3578973694
Peta Jeannie <Peta.jea@onja.org>
2020-06-05 11:26:37 +0300

    solved the first question

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,3 +1,11 @@
-const myArray = ["name","plants", "sky", "earth", "age", "gender", "animal", "bird", "people"];
-const randomArray = myArray[Math.floor(Math.random() * Math.length) | 2];
-console.log(randomArray);
+let firstArray = ["name","plants", "sky"];
+let secondArray = ["earth", "age", "gender"];
+let lastArray = ["animal", "bird", "people"];
+
+
+const randomArray_1 = firstArray[Math.floor(Math.random() * firstArray.length)];
+const randomArray_2 = secondArray[Math.floor(Math.random() * secondArray.length)];
+const randomArray_3 = lastArray[Math.floor(Math.random() * lastArray.length)];
+
+let quotes = `${randomArray_1}, ${randomArray_2} and ${randomArray_3} `;
+console.log(quotes);
