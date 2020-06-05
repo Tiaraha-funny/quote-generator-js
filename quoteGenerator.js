6bc91b882eb4927cba20c92d44628ae76f3fd2ad
Peta Jeannie <Peta.jea@onja.org>
2020-06-05 10:26:52 +0300

    fixed the array and use the math.random to make it work

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,23 +1,3 @@
-let ourquote = prompt("Give your quote here: ");
-
-let quotes = {
-
-firstQuote: {
-    minQuote: "my name is Petah",
-    seQuote: "I am quite old",
-    maxQuote: "I am living on earth"
-},
-
-secondQuote: {
-    oneQuote: "She is mamy",
-    twoQuote: "not that old",
-    threeQuote: "living Andamasina"
-  },
-
-lastQuote: {
-    priQuote: "He is a boy",
-    deQuote: "very very old",
-    preQuote: "somewhere on the sky"
-  }
-}; console.log(quotes(`Our quotes are ${firstQuote} which is about myself, ${secondQuote} that is a young girl and our last quote is ${lastQuote}. Those are our quotes.`))
-
+const myArray = ["name","plants", "sky", "earth", "age", "gender", "animal", "bird", "people"];
+const randomArray = myArray[Math.floor(Math.random() * Math.length) | 2];
+console.log(randomArray);
