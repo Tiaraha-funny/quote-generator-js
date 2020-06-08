719cff9c06d541e8902141020c7cc64f385c1dcf
Peta Jeannie <Peta.jea@onja.org>
2020-06-08 12:56:04 +0300

    tried to answer the second question

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
-# Onja Javascript Project 1 : These what I did in my test.
+# Javascript Project 
 
 We create our own code in this quoteGEnerator.js.
 
-
 ## About this test
 
-It is pretty tough test. I just make some codes. It is quite hard to understand but try to read the comments and you will get it clearly
+It is pretty tough test. I just make some codes and put some comments to help us to understand much better. It is quite hard to understand but try to read the comments and you will get it clearly
 
 ## My interpretation about this test
 
-I could not understand it but I did my best to achieve it.
+Always try to solve problem and it is nice to ask the teacher for help much rather than stay and do nothing.
+
+## This is the first part of this test
+
+## This is the second part of this test
+
+//1. use prompt to ask the user how many times we want to generate a random quote
+//2. check if the prompt give us something between 1 and 5
+//3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!
+//4. refactor our code into a function, and put the function call inside the loop
+//4.1 generate random numbers
+//4.2 show the generated quotes
 
 ## About this tips
 
--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,22 +1,53 @@
-let firstArray = ["name","plants", "sky"];
-let secondArray = ["earth", "age", "gender"];
-let lastArray = ["animal", "bird", "people"];
+let array1 = [
+  'Amateurs look for inspiration;',
+  'We have to be honest about what we want',
+  'All my life, I kept waiting for things to get better',
+  'Accidents happen',
+  'Either be a good parent',
+];
+let array2 = [
+  'the rest of us just',
+  'and take risks rather than lie to ourselves',
+  'But a funny thing happened while I was waiting for the world to change',
+  'Our bones shatter, our skin splits, our hearts break',
+  'to your career or ',
+];
+let array3 = [
+  'get up and go to work.',
+  'and make excuses to stay in our comfort zone.',
+  "Because I didn't change it.",
+  'We burn, we drown, we stay alive.',
+  'just give your dream up for adoption.',
+];
+let randomIndex1 = Math.floor(Math.random() * array1.length);
+let randomIndex2 = Math.floor(Math.random() * array2.length);
+let randomIndex3 = Math.floor(Math.random() * array3.length);
 
+let string1 = array1[randomIndex1];
+let string2 = array2[randomIndex2];
+let string3 = array3[randomIndex3];
 
-const randomArray_1 = firstArray[Math.floor(Math.random() * firstArray.length)];
-const randomArray_2 = secondArray[Math.floor(Math.random() * secondArray.length)];
-const randomArray_3 = lastArray[Math.floor(Math.random() * lastArray.length)];
+let quote = `${string1} ${string2} ${string3}`;
 
-let quotes = `${randomArray_1}, ${randomArray_2} and ${randomArray_3} `;
-console.log(quotes);
+console.log(quote);
 
-let genNum = 5;
-for (let number of genNum) {
-  if (number > 1) {
-    return genNum.quotes(1);
-  } else if (number > 5) {
-    return genNum.quotes(2);
-  } else {
-    return genNum.quotes(0);
-  }
+//1. use prompt to ask the user how many times we want to generate a random quote
+//2. check if the prompt give us something between 1 and 5
+//3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!
+//4. refactor our code into a function, and put the function call inside the loop
+//4.1 generate random numbers
+//4.2 show the generated quotes
+
+var number = Number(prompt("Please enter a number from 1 to 5", ""));
+
+if (number >= 5) {
+  Number(prompt("Please enter a number from 1 to 5", ""));
+} else {
+    Number(prompt("Your number (" + number + ") is above 5. Please enter a number from 1 to 5", ""));
+}
+
+  for (let i = 0; i < number; number++ ) {
+  quotes = Math.floor(Math.random() * 5) + 1;
+  quotes = Math.floor(Math.random() * 5) + 1;
+  quotes = Math.floor(Math.random() * 5) + 1;
 }
